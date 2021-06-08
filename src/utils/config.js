e7f72180bf13398030e6204d3e78a46e25ca1e02
mnn <Matveih1@yandex.ru>
2021-06-08 14:24:15 +0300

    выкладываю на сервер

--- src/utils/config.js
+++ src/utils/config.js
@@ -3,9 +3,9 @@ export const MoviesApiUrl = 'https://api.nomoreparties.co/beatfilm-movies';
 //   ? 'https://api.mnn.nomoredomains.icu'
 //   : 'http://localhost:3000';
 
-//export const BASE_URL = 'https://api.mnn.nomoredomains.icu';
+export const BASE_URL = 'https://api.mnn.nomoredomains.icu';
 // export const BASE_URL = 'http://localhost:3000';
-export const BASE_URL = 'http://192.168.1.157:3000';
+// export const BASE_URL = 'http://192.168.1.157:3000';
 
 export default {
   MoviesApiUrl,
